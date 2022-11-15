04e292dbf2ee2f4e6a5e13869a947abff650a7c0
Jan Koupil <jan.koupil@gmail.com>
2022-11-15 15:34:06 +0100

    2022-11-15 For úvod

---
+++ 03_While_13_Break/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _03_While_13_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            int cislo;
+            string nacteno;
+
+            //do
+            //{
+            //    Console.WriteLine("Zadej číslo, nebo x pro ukončení.");
+            //    nacteno = Console.ReadLine();
+
+            //    if (int.TryParse(nacteno, out cislo))
+            //    {
+            //        suma += cislo;
+            //    }
+            //    else if (nacteno != "x")
+            //    {
+            //        Console.WriteLine("Chybné zadání");
+            //    }
+
+            //}
+            //while (nacteno != "x");
+
+            //Console.WriteLine("Součet byl {0}", suma);
+
+            while (true)
+            {
+                Console.WriteLine("Zadej číslo, nebo x pro ukončení.");
+                nacteno = Console.ReadLine();
+
+                if (nacteno == "x")
+                    break;
+
+                if (int.TryParse(nacteno, out cislo))
+                {
+                    suma += cislo;
+                }
+                else
+                {
+                    Console.WriteLine("Chybné zadání");
+                }
+
+            }
+
+            Console.WriteLine("Součet byl {0}", suma);
+        }
+    }
+}
---
+++ 04_For_01_Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04_For_01_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 1;
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 101; i += 10)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 100; i > -1; i += 10)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //když chci aby se něco stalo opakovaně - 6 krát
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Něco dělám");
+            }
+
+
+            //když chci aby se něco stalo opakovaně - N krát
+            int N = 20;
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Něco dělám");
+            }
+        }
+    }
+}
