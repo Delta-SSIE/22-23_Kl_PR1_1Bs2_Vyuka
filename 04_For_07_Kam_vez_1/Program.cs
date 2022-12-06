91e831e62934d9531b40e54c5acd2dedf1b70269
Jan Koupil <jan.koupil@delta-skola.cz>
2022-12-06 12:38:52 +0100

    2022-12-06 For - "sachove" ulohy

---
+++ 04_For_07_Kam_vez_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _04_For_07_Kam_vez_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            string vstup;
+            char radekFigury = ' ';
+            char sloupecFigury = ' ';
+            bool jeChyba;
+
+            //načíst (a zkontrolovat)
+            do
+            {
+                jeChyba = false;
+                Console.WriteLine("Zadej pozici věže: ");
+                vstup = Console.ReadLine().ToUpper();
+
+                //má to dva znaky?
+                if (vstup.Length != 2)
+                {
+                    Console.WriteLine("Chybný formát");
+                    jeChyba = true;
+                    continue; //běž na koncovou podmínku
+                }
+
+                //rozdělit
+                sloupecFigury = vstup[0];
+                radekFigury = vstup[1];
+
+                if (sloupecFigury < 'A' || sloupecFigury > 'H' || radekFigury < '1' || radekFigury > '8')
+                {
+                    Console.WriteLine("Chybný formát");
+                    jeChyba = true;
+                    continue; //běž na koncovou podmínku
+                }
+            }
+            while (jeChyba);
+            //teď mám zajištěn platný vstup v obou proměnných
+
+
+            Console.WriteLine("Vež útočí na: ");
+            //projít všechny sloupce a přidat řádek věže
+            for (int i = 0; i < 8; i++)
+            {
+                char znakSloupce = (char)('A' + i);
+                if (znakSloupce != sloupecFigury)
+                    Console.WriteLine("" + znakSloupce + radekFigury);
+            }
+            //projít všechny řádky a přidat sloupec věže
+            for (int i = 0; i < 8; i++)
+            {
+                char znakRadku = (char)('1' + i);
+                if (znakRadku != radekFigury)
+                    Console.WriteLine("" + sloupecFigury + znakRadku);
+            }
+        }
+    }
+}
---
+++ 04_For_08_Kam_vez_2/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _04_For_08_Kam_vez_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup;
+            char radekFigury = ' ';
+            char sloupecFigury = ' ';
+            bool jeChyba;
+
+            //načíst (a zkontrolovat)
+            do
+            {
+                jeChyba = false;
+                Console.WriteLine("Zadej pozici věže: ");
+                vstup = Console.ReadLine().ToUpper();
+
+                //má to dva znaky?
+                if (vstup.Length != 2)
+                {
+                    Console.WriteLine("Chybný formát");
+                    jeChyba = true;
+                    continue; //běž na koncovou podmínku
+                }
+
+                //rozdělit
+                sloupecFigury = vstup[0];
+                radekFigury = vstup[1];
+
+                if (sloupecFigury < 'A' || sloupecFigury > 'H' || radekFigury < '1' || radekFigury > '8')
+                {
+                    Console.WriteLine("Chybný formát");
+                    jeChyba = true;
+                    continue; //běž na koncovou podmínku
+                }
+            }
+            while (jeChyba);
+            //teď mám zajištěn platný vstup v obou proměnných
+
+
+            int cisloRadkuFigury = radekFigury - '1';
+            int cisloSloupceFigury = sloupecFigury - 'A';
+
+            //vykreslím šachovnici
+            for (int y = 7; y >= 0; y--)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    if ((x+y) % 2 == 0)
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    else
+                        Console.BackgroundColor = ConsoleColor.White;
+
+                    if (y == cisloRadkuFigury && x == cisloSloupceFigury)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("R ");
+                    }
+                    else if (y == cisloRadkuFigury || x == cisloSloupceFigury)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write("# ");
+                    }
+                    else
+                    { 
+                        Console.Write("  ");
+                    }
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
