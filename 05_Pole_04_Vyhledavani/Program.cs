036bc8e56841b0da98524d6e8ac47be4d297aa82
Jan Koupil <jan.koupil@gmail.com>
2022-12-20 20:14:21 +0100

    2022-12-20 Pole - vyhledávání

---
+++ 05_Pole_04_Vyhledavani/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05_Pole_04_Vyhledavani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = new int[] { -11, 0, 5, -12, 13, 24, 19, 13, -17 };
+
+            int hledane = int.Parse(Console.ReadLine());
+
+            //bool naselJsem = false;
+            int index = -1;
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                int cislo = cisla[i];
+                if (cislo == hledane)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index > -1)
+                Console.WriteLine("Hledané číslo v poli je na pozici {0}.", index);
+            else
+                Console.WriteLine("Hledané číslo v poli není.");
+
+        }
+    }
+}
